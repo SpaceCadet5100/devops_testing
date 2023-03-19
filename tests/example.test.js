dfd15bf2c72f9ee35f504b1bfe67e3cde38b19ae
j.haberka-dawidek@student.avans.nl <j.haberka-dawidek@student.avans.nl>
2023-03-19 15:13:36 +0100

    minor changes

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,8 +1,8 @@
 //import something
 
 describe('test that will fail', () => {
-	test('1 + 4 = 2', () => {
-		const result =	(1 + 4);
+	test('1 + 4 = 3', () => {
+		const result =	(1 + 5);
 		expect(result).toEqual(2);
 	});
 });
