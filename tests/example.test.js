2d475dd63d3f8245dfb6cada58d6ad624d894a3c
Jedrek Dawidek <82816506+SpaceCadet5100@users.noreply.github.com>
2023-03-19 18:57:39 +0100

    Update example.test.js

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,7 +2,7 @@
 
 describe('test that will fail', () => {
 	test('1 + 4 = 5', () => {
-		const result =	(1 + 4);
-		expect(result).toEqual(5);
+		const result =	(2 + 4);
+		expect(result).toEqual(6);
 	});
 });
